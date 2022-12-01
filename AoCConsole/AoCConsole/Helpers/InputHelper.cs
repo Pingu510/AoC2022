a413c4264e4ee4eb4a7e3b136aaf8cebe1652607
Frida Gidlund <frida.gidlund@sogeti.com>
2022-12-01 19:01:33 +0100

    day 1

--- AoCConsole/AoCConsole/Helpers/InputHelper.cs
+++ AoCConsole/AoCConsole/Helpers/InputHelper.cs
@@ -10,7 +10,8 @@ namespace AoCConsole.Helpers
     {
         public static string[] GetInput(string fileName)
         {
-            return File.ReadAllLines(fileName);
+            string path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName + "/Inputs/", fileName);
+            return File.ReadAllLines(path);
         }
 
         public static List<int> ConvertToList(this string[] input)
@@ -20,7 +21,27 @@ namespace AoCConsole.Helpers
 
         public static List<(int index, List<int> group)> ConvertToListGroup(this string[] input)
         {
-            return new List<(int index, List<int> group)>();
+            var groupedInput = new List<(int index, List<int> group)>();
+            var group = new List<int>();
+            int elfIndex = 0;
+
+            foreach (var row in input)
+            {
+                if (string.IsNullOrWhiteSpace(row))
+                {
+                    groupedInput.Add((elfIndex, group));
+                    group = new List<int>();
+                    elfIndex++;
+                    continue;
+                }
+                else
+                {
+                    group.Add(int.Parse(row.Trim()));
+                }
+            }
+            groupedInput.Add((elfIndex, group));
+
+            return groupedInput;
         }
     }
 }
--- AoCConsole/AoCConsole/days/day1.cs
+++ AoCConsole/AoCConsole/days/day1.cs
@@ -11,16 +11,65 @@ namespace AoCConsole.Days
     {
         public day1()
         {
-            StarOne(InputHelper.GetInput("day1_1.txt"));
+            StarTwo(InputHelper.GetInput("test.txt"));
+            StarTwo(InputHelper.GetInput("day1_1.txt"));
 
         }
 
         public void StarOne(string[] input)
         {
-            var result = 0;
+            double highestSum = 0;
+            double elfSum = 0;
+            var groupedInputs = InputHelper.ConvertToListGroup(input);
 
-            Console.WriteLine("Result: " + result);
+            foreach (var elf in groupedInputs)
+            {
+                foreach (var cal in elf.group)
+                {
+                    elfSum += cal;
+                }
+
+                highestSum = highestSum > elfSum ? highestSum : elfSum;
+                elfSum = 0;
+            }
+
+            Console.WriteLine("Result: " + highestSum); // 69836
         }
 
+        public void StarTwo(string[] input)
+        {
+            var groupedInputs = InputHelper.ConvertToListGroup(input);
+            var topList = new List<(int elf, double sum)>(3) { (0, 0), (0, 0), (0, 0) };
+
+            foreach (var elf in groupedInputs)
+            {
+                double currentElfSum = 0;
+                foreach (var cal in elf.group)
+                {
+                    currentElfSum += cal;
+                }
+
+                int topListIndex = 3;
+                for (int i = 2; i >= 0; i--)
+                {
+                    if (currentElfSum > topList[i].sum)
+                    {
+                        topListIndex--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                if (topListIndex < 3)
+                {
+                    topList.Insert(topListIndex, (elf.index, currentElfSum));
+                }
+            }
+
+            var sum = topList[0].sum + topList[1].sum + topList[2].sum;
+            Console.WriteLine("Result: " + sum); // 69836
+        }
     }
 }
