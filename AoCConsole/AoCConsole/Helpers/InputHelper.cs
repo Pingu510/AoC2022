3bfd63fea5ea077cf1e2615cfc03a812bd84aefe
Frida Gidlund <frida.gidlund@sogeti.com>
2022-12-01 19:01:20 +0100

    Day 1 complete

---
+++ AoCConsole/AoCConsole/Helpers/InputHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCConsole.Helpers
+{
+    public static class InputHelper
+    {
+        public static string[] GetInput(string fileName)
+        {
+            return File.ReadAllLines(fileName);
+        }
+
+        public static List<int> ConvertToList(this string[] input)
+        {
+            return new List<int>();
+        }
+
+        public static List<(int index, List<int> group)> ConvertToListGroup(this string[] input)
+        {
+            return new List<(int index, List<int> group)>();
+        }
+    }
+}
--- AoCConsole/AoCConsole/days/day1.cs
+++ AoCConsole/AoCConsole/days/day1.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AoCConsole.Helpers;
 
-namespace AoCConsole.days
+namespace AoCConsole.Days
 {
-    internal class day1
+    public class day1
     {
+        public day1()
+        {
+            StarOne(InputHelper.GetInput("day1_1.txt"));
+
+        }
+
+        public void StarOne(string[] input)
+        {
+            var result = 0;
+
+            Console.WriteLine("Result: " + result);
+        }
+
     }
 }
