39c3928905e3fc95806ae7610892a94dfa4bb2ca
Frida Gidlund <frida_gidde95@hotmail.com>
2022-12-13 23:55:54 +0100

    prepwork for 12, 13, 14

--- AoCConsole/AoCConsole/Days/Day12.cs
+++ AoCConsole/AoCConsole/Days/Day12.cs
@@ -2,31 +2,68 @@
 
 namespace AoCConsole.Days
 {
-    /// <summary>
-    /// prep work
-    /// </summary>
-    internal class Day12
-    {
-        internal Day12()
-        {
-            Console.WriteLine("Day 12:");
+	/// <summary>
+	/// prep work
+	/// </summary>
+	internal class Day12
+	{
+		internal Day12()
+		{
+			Console.WriteLine("Day 12:");
 
-            StarOne(InputHelper.GetInput("test.txt")); // answer: 
-            StarTwo(InputHelper.GetInput("test.txt")); // answer: 
-        }
+			StarOne(InputHelper.GetInput("test.txt")); // answer: 
+			StarTwo(InputHelper.GetInput("test.txt")); // answer: 
+		}
 
-        private void StarOne(string[] input)
-        {
-            string result = "";
+		private void StarOne(string[] input)
+		{
+			char start = 'S';//a
+			char end = 'E';//z
 
-            Console.WriteLine("Result: " + result);
-        }
+			// populate list
+			var matrix = GetCharMatrix(input);
 
-        private void StarTwo(string[] input)
-        {
-            string result = "";
+			// recur the shaite out of it
+			// find shortest, skip loops
+			// test = 31 steps
 
-            Console.WriteLine("Result: " + result);
-        }
-    }
+			string result = "";
+
+			Console.WriteLine("Result: " + result);
+		}
+
+		List<List<FunTimesTM>> GetCharMatrix(string[] input)
+		{
+			var matrix = new List<List<FunTimesTM>>();
+			foreach (var row in input)
+			{
+				var funRow = new List<FunTimesTM>();
+				foreach (var letter in row)
+				{
+					funRow.Add(new FunTimesTM(letter));
+				}
+				matrix.Add(funRow);
+			}
+
+			return matrix;
+		}
+
+		private void StarTwo(string[] input)
+		{
+			string result = "";
+
+			Console.WriteLine("Result: " + result);
+		}
+
+		class FunTimesTM
+		{
+			public FunTimesTM(char letter)
+			{
+				Letter = letter;
+			}
+
+			public bool Dirty { get; set; }
+			public char Letter { get; set; }
+		}
+	}
 }
---
+++ AoCConsole/AoCConsole/Days/Day13.cs
@@ -0,0 +1,32 @@
+﻿using AoCConsole.Helpers;
+
+namespace AoCConsole.Days
+{
+	/// <summary>
+	/// prep work
+	/// </summary>
+	internal class Day13
+	{
+		internal Day13()
+		{
+			Console.WriteLine("Day preview:");
+
+			StarOne(InputHelper.GetInput("test.txt")); // answer: preview
+			StarTwo(InputHelper.GetInput("test.txt")); // answer: preview
+		}
+
+		private void StarOne(string[] input)
+		{
+			string result = "";
+
+			Console.WriteLine("Result: " + result);
+		}
+
+		private void StarTwo(string[] input)
+		{
+			string result = "";
+
+			Console.WriteLine("Result: " + result);
+		}
+	}
+}
